b9beecb7266f316727132d859d8dd214e160b8df
fabianosalles <fabiano.salles@gmail.com>
2024-07-04 21:18:09 -0300

    back, forward, refresh and getTitle command added

---
+++ src/shared/RTA.Core/WebDriver/Commands/Forward/ForwardCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace RTA.Core.WebDriver.Commands.ForwardCommand;
+
+/// <summary>
+/// https://www.w3.org/TR/webdriver2/#forward
+/// </summary>
+/// <param name="settings"></param>
+/// <param name="client"></param>
+/// <param name="sessionId"></param>
+public class ForwardCommand(Settings settings, HttpClient client, string sessionId) : ICommand<bool>
+{
+    public async Task<bool> RunAsync()
+    {
+        var url = $"http://localhost:{settings.Port}/session/{sessionId}/forward";
+        var response = await client.PostAsync(url, null);
+        return response.IsSuccessStatusCode;
+    }
+}
---
+++ src/shared/RTA.Core/WebDriver/Commands/Refresh/RefreshCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace RTA.Core.WebDriver.Commands.RefreshCommand;
+
+/// <summary>
+/// https://www.w3.org/TR/webdriver2/#refresh
+/// </summary>
+/// <param name="settings"></param>
+/// <param name="client"></param>
+/// <param name="sessionId"></param>
+public class RefreshCommand(Settings settings, HttpClient client, string sessionId): ICommand<bool>
+{
+    public async Task<bool> RunAsync()
+    {
+        var url = $"http://localhost:{settings.Port}/session/{sessionId}/back";
+        var response = await client.PostAsync(url, null);
+        return response.IsSuccessStatusCode;
+    }
+}
